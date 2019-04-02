39ef6f3fef5d9eb39f407ee2a748f33cdae478a6
Blaine Lawson <blaine.lawson@gmail.com>
2019-04-02 16:31:30 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,7 +12,7 @@ class Scraper
     doc.css(".student-card").each do |student_card|
       student_hash = {
                           name: student_card.css("h4").text,
-                      location: student_card.css("p").text, 
+                      location: student_card.css("p").text,
                    profile_url: student_card.css("a").attr("href").value
                   }
       student_hash_array << student_hash
