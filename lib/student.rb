a54fc36ec280e50459bf461404dfd65d8781ad20
Blaine Lawson <blaine.lawson@gmail.com>
2019-04-02 17:51:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,9 +14,7 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    # binding.pry
     attributes_hash.each do |k, v|
-      # binding.pry
       self.send(("#{k}="), v)
     end
   end
