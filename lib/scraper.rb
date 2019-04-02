7b504a787125ee8c1bdbf6c1fa84e5aa7a2fc5d4
Blaine Lawson <blaine.lawson@gmail.com>
2019-04-02 15:27:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,10 +6,11 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = open(index_url)
     doc = Nokogiri::HTML(html)
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
-
+    doc.css()
   end
 
 end
