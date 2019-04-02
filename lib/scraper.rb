e2dbbb3ce2e1475cbbb6dfe6596dbf716192e039
Blaine Lawson <blaine.lawson@gmail.com>
2019-04-02 15:29:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = open(index_url)
     doc = Nokogiri::HTML(html)
-    binding.pry
+    # binding.pry
     doc.css(".roster-cards-container").each do |student_card|
       binding.pry
     end
